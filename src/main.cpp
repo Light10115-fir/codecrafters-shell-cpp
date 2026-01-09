3d1170a778e8fde16f869b66204090bc484666d0
Light10115 <light10115.free@gmail.com>
2026-01-10 00:22:16 +0200

    Implemented a Read Eval Print Loop (REPL)

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,12 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
-  std::string command;
-  std::cin >> command;
-  std::cout << command << ": command not found" << std::endl;
-}
+  // Start REPL
+  int start = 1;
+  while (start < 2){
+	  std::cout << "$ ";
+	  std::string command;
+	  std::cin >> command;
+	  std::cout << command << ": command not found" << std::endl;
+  }
+} 
