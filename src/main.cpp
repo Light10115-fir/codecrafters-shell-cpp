cc3f4d169e5961183ff5373e8f4b8618c6939477
Light10115 <light10115.free@gmail.com>
2026-01-10 16:14:36 +0200

    fixed echo to not take whitespace at start of string

--- src/main.cpp
+++ src/main.cpp
@@ -9,19 +9,19 @@ int main() {
   // Start REPL
   std::string command;
   std::string echo_arguments;
-  while (command != "exit"){
+  while (true){
 	  std::cout << "$ ";
 	  std::string command;
-	  std::cin >> command;
-	  if (command != "exit" && command != "echo") {
-		  std::cout << command << ": command not found" << std::endl;
+	  std::getline (std::cin, command);
+	  if (command == "exit") {
+		  break;
 	  }
-	  else if (command == "echo") {
-		  std::getline(std::cin, echo_arguments);
+	  else if (command.substr(0,4) == "echo") {
+		  std::cout << command.substr(5) << std::endl;
 		  std::cout << echo_arguments << "\n";
 	  }
 	  else {
-		  return 0;
+		  std::cout << command << ": command not found" << std::endl;
 	  }
   }
 } 
